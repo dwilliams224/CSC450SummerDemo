417730c3e52961b43e319e815efed29c5132f237
Darren Williams <darrenwilliams@127.0.0.1>
2017-07-30 14:09:41 -0500

    Add Data Manager class

---
+++ Demo/Assets/Code/Data Selection/DataPointManager.cs
@@ -0,0 +1,15 @@
+﻿/*	DataPointManager.cs
+ * 
+ * 	Summary: The Datapoint manager is a singleton responsable for data injection 
+ * 	from the Table Scene to the Simple Forest Scene.
+ * 
+ * 	© Darren Williams 2017
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataPointManager : MonoBehaviour {
+
+
+}
